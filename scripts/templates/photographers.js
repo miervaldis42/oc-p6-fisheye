8175736274ee0bae21522281fd8bbe9f78659db3
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2023-12-24 18:28:45 +0400

    feat (homePage) : Photographers Article Update
    - Use object descontruction to retrieve other information from the retrieve JSON file data
    - Add new elements to the `<article>` tag to display more information about each photographer
    - Link the data to the UI elements

--- scripts/templates/photographers.js
+++ scripts/templates/photographers.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-  const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
   const picture = `assets/photographers/photos/${portrait}`;
 
@@ -16,8 +16,25 @@ function photographerTemplate(data) {
     const h2 = document.createElement("h2");
     h2.textContent = name;
 
+    // City & Country
+    const location = document.createElement("p");
+    location.className = "location";
+    location.textContent = `${city}, ${country}`;
+
+    // Tagline
+    const slogan = document.createElement("p");
+    slogan.textContent = tagline;
+
+    // Man-Day Price
+    const dayPrice = document.createElement("p");
+    dayPrice.className = "price";
+    dayPrice.textContent = `${price}€/jour`;
+
     article.appendChild(img);
     article.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(slogan);
+    article.appendChild(dayPrice);
 
     return article;
   }
