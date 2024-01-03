1277b01253402b5889ba2607629630f28bff0722
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2024-01-03 17:09:19 +0400

    refactor (homePage) : Dynamic Home Page Builder
    - Create dynamically the main content of the Home page
    - Change the function name to understand its purpose
    - Add & Translate the comments in English

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,9 +2,17 @@
 import photographerTemplate from "../templates/photographers";
 import { getPhotographers } from "../utils/fetchData";
 
-async function displayData(photographers) {
-  const photographersSection = document.querySelector(".photographer-list");
+async function buildPhotographersList(photographers) {
+  const main = document.querySelector("#main");
 
+  // Section containing the list of photographers
+  const photographersSection = document.createElement("section");
+  photographersSection.id = "photographer-list";
+  photographersSection.ariaLabel = "A list of photographers you can hire.";
+
+  main.appendChild(photographersSection);
+
+  // Build an clickable article for each photographer
   photographers.forEach((photographer) => {
     const photographerModel = photographerTemplate(photographer);
     const userCardDOM = photographerModel.getUserCardDOM();
@@ -13,8 +21,8 @@ async function displayData(photographers) {
 }
 
 async function init() {
-  // Récupère les datas des photographes
+  // Retrieve data from database
   const { photographers } = await getPhotographers();
-  displayData(photographers);
+  buildPhotographersList(photographers);
 }
 init();
